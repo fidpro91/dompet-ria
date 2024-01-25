d5c8b7c031c29b8c5a0e33edff333946515ef5db
fidpro91 <ufi.alfi@gmail.com>
2024-01-25 16:29:03 +0700

    updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,7 +28,6 @@ class HomeController extends Controller
             ->backgroundcolor("rgb(30, 211, 202)");
 
         $data   = Pencairan_jasa_header::where("is_published",1)->latest()->first();
-        $chart['last_remun'] = null;
         if ($data) {
             $chart['last_remun']  = new RemunChart;
             $dataPercent   = DB::table("persentase_jasa")->where("id_cair",$data->id_cair_header);
